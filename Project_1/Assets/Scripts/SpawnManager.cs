2af5b8aa6e206cf759c66ff1fa033a06e887276b
Murtazina Anastasia <nastya.murtazina.00@inbox.ru>
2023-09-12 00:20:13 +0300

    Created a scene and increaseButton
    - created the scene with the addition of the necessary assets;
    - added a panel for displaying FPS;
    - added the 4 buttons (start and restart FPS calculation, adding and deleting 100 objects;
    - set up a prefab (cookie);
    - сreated the functionality of the button for generating +100 objects with a random position.

---
+++ Project_1/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.SocialPlatforms;
+using Random = UnityEngine.Random;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject cookiePrefab;
+    public List<GameObject> cookiesContainer;
+    public GameObject startButton;
+    public GameObject restartButton;
+    public GameObject increaseButton;
+    public GameObject reduceButton;
+    
+    private const float XMax = 13;
+    private const float YMax = 10;
+    private const float ZPosition = -0.5f;
+
+    [SerializeField] private int maxCountObject = 500;
+    private int minCountObject = 0;
+
+    
+    public void StartSpawn()
+    {
+        increaseButton.SetActive(true);
+        AddPrefabs();
+    }
+
+    private void AddPrefabs()
+    {
+        var hundred = GetHundredCookies();
+        cookiesContainer.AddRange(hundred);
+    }
+    
+    private IEnumerable<GameObject> GetHundredCookies()
+    {
+        var cookieArray = new GameObject[100];
+        for (int i = 0; i < 100; i++)
+        {
+            CreateCookiePrefab(cookiePrefab);
+        }
+        return cookieArray;
+    }
+    
+    private void CreateCookiePrefab(GameObject cookie)
+    {
+        Vector3 newPosition = new Vector3(
+            Random.Range(-XMax, XMax),
+            Random.Range(-YMax, YMax),
+            ZPosition);
+        Instantiate(cookie, newPosition, Quaternion.identity);
+    }
+    
+
+    private void DeleteAllObject()
+    {
+    }
+}
