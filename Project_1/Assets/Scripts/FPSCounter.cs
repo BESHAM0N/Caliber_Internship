4dce297f75c80b5788f82c415eab9d0274e53499
Murtazina Anastasia <nastya.murtazina.00@inbox.ru>
2023-09-15 02:54:34 +0300

    bug fixing and refactoring
    fixing a performance error (permanent creation of a new array)

--- Project_1/Assets/Scripts/FPSCounter.cs
+++ Project_1/Assets/Scripts/FPSCounter.cs
@@ -1,82 +1,77 @@
 using UnityEngine;
 using System;
 using System.Linq;
-
-
 public class FPSCounter : MonoBehaviour
 {
     private const float _fivePercentile = 0.05f;
     private const float _onePercentile = 0.01f;
-    private const int _bufferStepCapacity = 5000;
+    private const int _bufferStepCapacity = 1000;
+    private const float _CurrentRepeatRate = 0.12f;
+    private const float _AverageRepeatRate = 0.5f;
+    private const float _PercentileRepeatRate = 0.5f;
     
     private int[] _fpsBuffer = new int[_bufferStepCapacity];
     private int _fpsBufferIndex;
-    private int _bufferCount;
     private int _currentFpsBuffer;
     public int CurrentFps { get; private set; }
     public int AverageFps { get; private set; }
     public double FivePercentile { get; private set; }
     public double OnePercentile { get; private set; }
-
+    
     public void Start()
     {
-        InvokeRepeating("SetCurrentFps", 0, 0.2f);
-        InvokeRepeating("CalculateAverageFps", 0f, 0.5f);
-        InvokeRepeating("CalculatePercentile", 0f, 1f);
+        InvokeRepeating("SetCurrentFps", 0f, _CurrentRepeatRate);
+        InvokeRepeating("CalculateAverageFps", 0f, _AverageRepeatRate);
+        InvokeRepeating("CalculatePercentile", 0f, _PercentileRepeatRate);
     }
     
     public void Update()
     {
-        _currentFpsBuffer = GetCurrentFps();
-        InsertInFpsBuffer(_currentFpsBuffer);
+        CalculateCurrentFps();
     }
     
-    private int GetCurrentFps()
-    {
-        return (int)(1f / Time.unscaledDeltaTime);
-    }
-
     private void SetCurrentFps()
     {
         CurrentFps = _currentFpsBuffer;
     }
-
-    private void InsertInFpsBuffer(int currentFps)
+    
+    private void CalculateCurrentFps()
     {
-        _fpsBuffer[_fpsBufferIndex++] = currentFps;
-        _bufferCount = _fpsBufferIndex++;
-
-        if (_fpsBufferIndex >= _bufferCount)
+        var fps = (int)Math.Round(1f / Time.unscaledDeltaTime);
+        _currentFpsBuffer = fps;
+        _fpsBuffer[_fpsBufferIndex] = fps;
+        _fpsBufferIndex++;
+    
+        if (_fpsBufferIndex >= _fpsBuffer.Length)
             Array.Resize(ref _fpsBuffer, _fpsBuffer.Length + _bufferStepCapacity);
     }
-
+    
     private int CalculateAverageFps()
     {
         int sum = _fpsBuffer.Sum();
         if (sum > 0)
-            AverageFps = sum / _bufferCount;
+            AverageFps = sum / _fpsBufferIndex+1;
         return AverageFps;
     }
-
+    
     private void CalculatePercentile()
     {
-        var orderedFps = _fpsBuffer.Where(i => i > 0).OrderBy(n => n).ToArray();
+        var orderedFps = _fpsBuffer.Where(n => n > 0).OrderBy(n => n).ToArray();
         if (orderedFps.Any())
         {
-            var fivePercentileIndex = (int)Math.Round(_fivePercentile * _bufferCount);
-            var onePercentileIndex = (int)Math.Round(_onePercentile * _bufferCount);
-            FivePercentile = orderedFps[fivePercentileIndex];
+            var onePercentileIndex = (int)Math.Round(_onePercentile * _fpsBufferIndex);
+            var fivePercentileIndex = (int)Math.Round(_fivePercentile * _fpsBufferIndex);
             OnePercentile = orderedFps[onePercentileIndex];
+            FivePercentile = orderedFps[fivePercentileIndex];
         }
     }
-
+    
     public void RestartCount()
     {
-        _fpsBuffer = new int[1000];
-        AverageFps = 0;
-        FivePercentile = 0;
-        OnePercentile = 0;
-        _bufferCount = 0;
+        _fpsBuffer = new int[_bufferStepCapacity];
         _fpsBufferIndex = 0;
+        AverageFps = 0;
+        OnePercentile = 0;
+        FivePercentile = 0;
     }
 }
